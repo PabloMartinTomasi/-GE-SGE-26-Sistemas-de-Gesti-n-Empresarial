4a90576b25bb3cd026db5633c1047b59b946714e
PabloMartinTomasi <pablo.martintomasi24@campusfp.es>
2026-01-27 12:26:39 +0100

    asd

--- Python/POO_PabloMartinTomasi/ejerciciosRepaso.py
+++ Python/POO_PabloMartinTomasi/ejerciciosRepaso.py
@@ -382,23 +382,3 @@ print(f"{estudiante.es_mayor_de_edad()}") """
 
 
 
-class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
-    
-    def set_nombre(self, nombre):
-        self.nombre = nombre
-    def get_nombre(self):
-        return self.nombre
-    
-    def set_edad(self, edad):
-        self.edad = edad
-    def get_edad(self):
-        return self.edad
-    
-
-
-
-
-
